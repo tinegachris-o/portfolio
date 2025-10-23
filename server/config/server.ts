f713dc16eb3eac46755d506471911def527733fa
tinegachris-o <tinegachris797@gmail.com>
2025-10-23 13:58:48 +0300

    Fix secure cookie issue for Render

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,20 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+
+  url: env("PUBLIC_URL", "https://portfolio-f9nx.onrender.com"), // important for admin assets
+  proxy: true, // trust Render's HTTPS proxy
+
   app: {
     keys: env.array("APP_KEYS"),
+  },
+
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    cookies: {
+      secure: true, // secure cookies are fine when behind HTTPS
+    },
   },
 });
