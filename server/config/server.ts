acc1c57a2157f8f43e7ca6ad537c2c12a582bf70
tinegachris-o <tinegachris797@gmail.com>
2025-10-23 08:41:57 +0300

    setting cookies

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,7 +1,15 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    cookies: {
+      secure: false, // 🩵 Fixes "Cannot send secure cookie over unencrypted connection"
+    },
   },
 });
