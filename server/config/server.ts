dbfe13646d70ba1709cebaf8fe3e47c813411b30
tinegachris-o <tinegachris797@gmail.com>
2025-10-23 13:43:34 +0300

    Fix secure cookie issue for Render

--- server/config/server.ts
+++ server/config/server.ts
@@ -4,19 +4,4 @@ export default ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-      sessions: {
-        maxSessionLifespan: 1000 * 24 * 60 * 60 * 1000, // 1000 days (in milliseconds)
-        maxRefreshTokenLifespan: 1000 * 24 * 60 * 60 * 1000, // 1000 days
-      },
-    },
-    cookies: {
-      secure: true, // ✅ we want secure cookies in production
-    },
-  },
-  settings: {
-    proxy: true, // 🧩 tells Strapi to trust Render's HTTPS proxy
-  },
 });
